5a03bc39825454a3ebfe8067da5d3ac4671da4b1
sharundas <123480220+sharundas@users.noreply.github.com>
2023-12-16 22:48:43 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+      
+  )
+}
+
+export default App
