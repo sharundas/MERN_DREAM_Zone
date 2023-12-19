3948882e73968e51528f0532493a0e75bb2b0d78
sharundas <123480220+sharundas@users.noreply.github.com>
2023-12-19 08:27:09 +0530

    connected to DB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 
 const app = express();
@@ -7,3 +22,8 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
+
+
+// mern_dream_zone - username
+// uVDkNhaZ3wdAr5lp - pass mongodb
